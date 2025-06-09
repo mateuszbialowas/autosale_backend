5e1deed05cb87d64dff36c7eabdbc8ce7c7f5542
mateuszbialowas <mateusz.bialowas2@gmail.com>
2025-06-09 21:58:03 +0200

    Authenticate avo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admins
-  mount_avo
+
+  authenticate :admin do
+    mount_avo
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
