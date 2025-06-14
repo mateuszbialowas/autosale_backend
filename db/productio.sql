8fe3e1aed188ad683b8f346bf41eef8cfcf5d98e
mateuszbialowas <mateusz.bialowas2@gmail.com>
2025-06-14 17:30:58 +0200

    setup kamal deploy

---
+++ db/productio.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE autosale_backend_production;
+CREATE DATABASE autosale_backend_production_cache;
+CREATE DATABASE autosale_backend_production_queue;
+CREATE DATABASE autosale_backend_production_cable;
